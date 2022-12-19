c93b395c1b27b64ca6b8dfe80b44ddd05a39e5bd
nisheri-ascar <kertkert704@gmail.com>
2022-12-19 05:30:15 +0000

    Partial Refactoring and add stuff
    LAST WORKING STATE! TOOK ABOUT DAYS TO FIND STUPID WAY TO DO IT ACTUALLY

--- bot.js
+++ bot.js
@@ -1,24 +1,19 @@
 require("dotenv").config()
-// Load discord.js
-const {
-  Client,
-  Intents
-} = require('discord.js')
+
+const Discord = require('discord.js');
 // Create Discord intentions, required in v13
-const intents = new Intents(['GUILDS', 'GUILD_MESSAGES'])
+//const intents = new Intents(['GUILDS', 'GUILD_MESSAGES']);
 // Create Discord client
-const client = new Client({
-  intents
-})
+const client = new Discord.Client();
+
 
 let channel = process.env.channel
 
-// Load mineflayer
 const mineflayer = require('mineflayer')
 const bot = mineflayer.createBot({ //change these parts if needed
   host: "localhost",
   port: 25565,
-  username: 'DiscordBridge'
+  username: 'Bot'	
 })
 
 client.on('ready', () => {
@@ -33,25 +28,39 @@ client.on('ready', () => {
 
 // Discord -> in-game chat
 client.on('message', (message) => {
-  // Only handle messages in specified channel
-  if(message.channel.id !== channel.id){
-	  return
-  } if(message.author.id === client.user.id) {
-	  return
-  } if(message.channel.id === channel.id) {
-   console.log(`Discord: ${message.author.username}: ${message.content}`);
-   bot.chat(`${message.author.username}: ${message.content}`);
-	}
-})
+ //Todo: Rewrite whole shit in case
+ //In-discord commands
+ /* if(message.content[0] === "!") {
+	  switch(message.content){
+		  case "!help":
+			  channel.send("Currently working on it..");
+		  case "!status"
+			  channel.send(`{message.author.username} What the fuck do you expect?`);
+	  }
+	} */
+  if(message.channel.id !== channel.id) return;
+  if(message.author.id === client.user.id) return;
+  if(message.channel.id === channel.id && message.content[0] !== "!"){
+	  console.log(`Discord: ${message.author.username}: ${message.content}`);
+	  bot.chat(`${message.author.username}: ${message.content}`);
+  	}
+});
+
+bot.on('playerJoined', (player) =>{
+	bot.chat(`${player.username} joined`);
+	if(player.username !== bot.username){
+		console.log(`{player.username} joined the game.`);
+		channel.send(`{player.username} joined the game.`);
+		bot.chat(`Welcome to Cult Of Mental Retardation Minecraft Server ${
+player.username}!, We hope you enjoy your stay here!`);
+	  }
+});
 
 // In-game messages -> Discord channel
 bot.on('chat', (username, message) => {
-  // Ignore messages from the bot itself
-  if (username === bot.username){
-	  return
-  } if (username !== bot.username){
-  console.log(`Minecraft: ${username}: ${message}`)
-  channel.send(`\`${username}\`: ${message}`)
+  if(username !== bot.username){
+  console.log(`Minecraft: ${username}: ${message}`);
+  channel.send(`\`${username}\`: ${message}`);
   }
-})
+});
 client.login(process.env.TOKEN);
---
+++ test.js
@@ -0,0 +1,9 @@
+const mineflayer = require('mineflayer');
+
+// Create a new Mineflayer bot
+const bot = mineflayer.createBot({
+  host: 'localhost', // Minecraft server host
+  port: 25565, // Minecraft server port
+  username: 'Lilita', // Minecraft username
+});
+
